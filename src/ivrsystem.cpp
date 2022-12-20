bebd3e65f2093b13de923d33ad9f4e28d4156594
danwillm <39023874+danwillm@users.noreply.github.com>
2022-12-20 11:17:14 +0000

    get property bindings

--- src/ivrsystem.cpp
+++ src/ivrsystem.cpp
@@ -4,6 +4,12 @@
 
 IVRSystem::IVRSystem(const Napi::CallbackInfo& info) : ObjectWrap(info){};
 
+#define CHECK_PROPERTY_ERROR(env, err)                                    \
+  if (err) {                                                              \
+    Napi::Error::New(env, vr::VRSystem()->GetPropErrorNameFromEnum(err)); \
+    return env.Null();                                                    \
+  }
+
 Napi::Value IVRSystem::GetControllerRoleForTrackedDeviceIndex(const Napi::CallbackInfo& info) {
   CHECK_NUM_ARGUMENTS(info, 1);
 
@@ -14,7 +20,59 @@ Napi::Value IVRSystem::GetControllerRoleForTrackedDeviceIndex(const Napi::Callba
   return Napi::Number::New(env, role);
 }
 
+Napi::Value IVRSystem::GetBoolTrackedDeviceProperty(const Napi::CallbackInfo& info) {
+  CHECK_NUM_ARGUMENTS(info, 2);
+
+  Napi::Env env = info.Env();
+
+  vr::ETrackedPropertyError err;
+  bool result = vr::VRSystem()->GetBoolTrackedDeviceProperty(
+      info[0].As<Napi::Number>().Int32Value(), static_cast<vr::ETrackedDeviceProperty>(info[0].As<Napi::Number>().Int32Value()), &err);
+  CHECK_PROPERTY_ERROR(env, err);
+
+  return Napi::Boolean::New(env, result);
+}
+
+Napi::Value IVRSystem::GetStringTrackedDeviceProperty(const Napi::CallbackInfo& info) {
+  CHECK_NUM_ARGUMENTS(info, 2);
+
+  Napi::Env env = info.Env();
+
+  vr::ETrackedPropertyError err;
+
+  char result[4096];
+  vr::VRSystem()->GetStringTrackedDeviceProperty(
+      info[0].As<Napi::Number>().Int32Value(),
+      static_cast<vr::ETrackedDeviceProperty>(info[0].As<Napi::Number>().Int32Value()),
+      result,
+      sizeof(result),
+      &err);
+  CHECK_PROPERTY_ERROR(env, err);
+
+  return Napi::String::New(env, result);
+}
+
+Napi::Value IVRSystem::GetInt32TrackedDeviceProperty(const Napi::CallbackInfo& info) {
+  CHECK_NUM_ARGUMENTS(info, 2);
+
+  Napi::Env env = info.Env();
+
+  vr::ETrackedPropertyError err;
+  int32_t result = vr::VRSystem()->GetInt32TrackedDeviceProperty(
+      info[0].As<Napi::Number>().Int32Value(), static_cast<vr::ETrackedDeviceProperty>(info[0].As<Napi::Number>().Int32Value()), &err);
+  CHECK_PROPERTY_ERROR(env, err);
+
+  return Napi::Number::New(env, result);
+}
+
 Napi::Function IVRSystem::GetClass(Napi::Env env) {
   return DefineClass(
-      env, "IVRSystem", {IVRSystem::InstanceMethod("GetControllerRoleForTrackedDeviceIndex", &IVRSystem::GetControllerRoleForTrackedDeviceIndex)});
+      env,
+      "IVRSystem",
+      {
+          IVRSystem::InstanceMethod("GetControllerRoleForTrackedDeviceIndex", &IVRSystem::GetControllerRoleForTrackedDeviceIndex),
+          IVRSystem::InstanceMethod("GetBoolTrackedDeviceProperty", &IVRSystem::GetBoolTrackedDeviceProperty),
+          IVRSystem::InstanceMethod("GetStringTrackedDeviceProperty", &IVRSystem::GetStringTrackedDeviceProperty),
+          IVRSystem::InstanceMethod("GetInt32TrackedDeviceProperty", &IVRSystem::GetInt32TrackedDeviceProperty),
+      });
 }
