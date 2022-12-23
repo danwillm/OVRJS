1b1997481d422d2a36b360f9c6c5b8400ba5e1e9
danwillm <39023874+danwillm@users.noreply.github.com>
2022-12-23 12:37:57 +0000

    fix typescript bindings

--- websrc/index.ts
+++ websrc/index.ts
@@ -1,9 +1,17 @@
-import * as headers from "./headers";
-import {ETrackedPropertyError} from "./headers";
+import {
+    ETrackedControllerRole,
+    ETrackedPropertyError,
+    ETrackingUniverseOrigin, EVRApplicationType, HmdMatrix34_t, TrackedDeviceIndex_t, TrackedDeviceProperty,
+    VREvent_t,
+    VROverlayError,
+    VROverlayFlags,
+    VROverlayInputMethod
+} from "./headers";
+import * as Buffer from "buffer";
 
 const ovr = require('bindings')('openvr');
 
-class VROverlay {
+export class VROverlay {
     private ivroverlay;
 
     private readonly handle: number;
@@ -14,15 +22,15 @@ class VROverlay {
         this.handle = this.ivroverlay.CreateOverlay(name, key);
     }
 
-    ShowOverlay() : headers.VROverlayError {
+    ShowOverlay() : VROverlayError {
         return this.ivroverlay.ShowOverlay(this.handle);
     }
 
-    SetOverlayWidthInMetres(width: number) : headers.VROverlayError {
+    SetOverlayWidthInMetres(width: number) : VROverlayError {
         return this.ivroverlay.SetOverlayWidthInMetres(width);
     }
 
-    SetOverlayTextureFromBuffer(buffer: Uint8ClampedArray, width: number, height: number) {
+    SetOverlayTextureFromBuffer(buffer: Buffer, width: number, height: number) {
         return this.ivroverlay.SetOverlayTextureFromBuffer(this.handle, buffer, width, height);
     }
 
@@ -31,58 +39,56 @@ class VROverlay {
         return this.ivroverlay.SetOverlayColor(this.handle, red, green, blue);
     }
 
-    SetOverlayTransformTrackedDeviceRelative(device_index: headers.TrackedDeviceIndex_t, transform: headers.HmdMatrix34_t) {
+    SetOverlayTransformTrackedDeviceRelative(device_index: TrackedDeviceIndex_t, transform: HmdMatrix34_t) {
         return this.ivroverlay.SetOverlayTransformTrackedDeviceRelative(this.handle, device_index, transform);
     }
 
-    SetOverlayTransformAbsolute(origin: headers.ETrackingUniverseOrigin, transform: headers.HmdMatrix34_t) {
+    SetOverlayTransformAbsolute(origin: ETrackingUniverseOrigin, transform: HmdMatrix34_t) {
         return this.ivroverlay.SetOverlayTransformAbsolute(this.handle, origin, transform);
     }
 
-    SetOverlayInputMethod(method: headers.VROverlayInputMethod) {
+    SetOverlayInputMethod(method: VROverlayInputMethod) {
         return this.ivroverlay.SetOverlayInputMethod(this.handle, method);
     }
 
-    SetOverlayFlag(flag: headers.VROverlayFlags) {
+    SetOverlayFlag(flag: VROverlayFlags) {
         return this.ivroverlay.SetOverlayFlag(this.handle, flag);
     }
 
-    PollNextOverlayEvent() : headers.VREvent_t | boolean {
+    PollNextOverlayEvent() : VREvent_t | boolean {
         return this.ivroverlay.PollNextOverlayEvent(this.handle);
     }
 }
 
-class VRSystem {
+export class VRSystem {
     private ivrsystem;
 
     constructor() {
         this.ivrsystem = new ovr.IVRSystem();
     }
 
-    GetControllerRoleForTrackedDeviceIndex(device_index: headers.TrackedDeviceIndex_t) : headers.ETrackedControllerRole {
+    GetControllerRoleForTrackedDeviceIndex(device_index: TrackedDeviceIndex_t) : ETrackedControllerRole {
         return this.ivrsystem.GetControllerRoleForTrackedDeviceIndex(device_index);
     }
 
-    GetBoolTrackedDeviceProperty(device_index: headers.TrackedDeviceIndex_t, property: headers.TrackedDeviceProperty) : boolean | ETrackedPropertyError {
+    GetBoolTrackedDeviceProperty(device_index: TrackedDeviceIndex_t, property: TrackedDeviceProperty) : boolean | ETrackedPropertyError {
         return this.ivrsystem.GetBoolTrackedDeviceProperty(device_index, property);
     }
 
-    GetInt32TrackedDeviceProperty(device_index: headers.TrackedDeviceIndex_t, property: headers.TrackedDeviceProperty) : number | ETrackedPropertyError {
+    GetInt32TrackedDeviceProperty(device_index: TrackedDeviceIndex_t, property: TrackedDeviceProperty) : number | ETrackedPropertyError {
         return this.ivrsystem.GetInt32TrackedDeviceProperty(device_index, property);
     }
 
-    GetFloatTrackedDeviceProperty(device_index: headers.TrackedDeviceIndex_t, property: headers.TrackedDeviceProperty) : number | ETrackedPropertyError {
+    GetFloatTrackedDeviceProperty(device_index: TrackedDeviceIndex_t, property: TrackedDeviceProperty) : number | ETrackedPropertyError {
         return this.ivrsystem.GetFloatTrackedDeviceProperty(device_index, property);
     }
 
-    GetStringTrackedDeviceProperty(device_index: headers.TrackedDeviceIndex_t, property: headers.TrackedDeviceProperty) : string | ETrackedPropertyError {
+    GetStringTrackedDeviceProperty(device_index: TrackedDeviceIndex_t, property: TrackedDeviceProperty) : string | ETrackedPropertyError {
         return this.ivrsystem.GetStringTrackedDeviceProperty(device_index, property);
     }
 }
 
-export default {
-    VROverlay,
-    VRSystem,
-    ...headers,
-    ...ovr
-};
+export const VR_Init = (application_type: EVRApplicationType) => ovr.VR_Init(application_type);
+
+export * from "./headers";
+export const bindings = ovr;
